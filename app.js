1bd76ac212008cfe38fd041d2b145b5ab1e899a8
Xiaobin Zhu <sam.zhu992@gmail.com>
2022-07-19 20:05:23 -0600

    start project

---
+++ app.js
@@ -0,0 +1,16 @@
+//import express modules and create server
+const express = require('express')
+const app = express()
+
+
+
+
+
+//prepare port and run server
+const port = 3333
+app.listen(port,console.log(`Server is listening port ${port}...`))
+
+
+
+
+
