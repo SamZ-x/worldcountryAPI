fc6158f8a12f09fab881d49af5aec82de76d705a
Xiaobin Zhu <sam.zhu992@gmail.com>
2022-07-19 22:54:01 -0600

    design routes and control functions

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
+
+//****  MODULES & INSTANCES ****//
+
 //import express modules and create server
 const express = require('express')
 const app = express()
 
+//import routes
+const countryRoutes = require('./routes/countryRoutes')
 
+//****  Middleware USE ****//
+app.use(express.json())
 
-
+//****  MODULES USE ****//
+app.use('/api/v1/worldcountrys', countryRoutes)
 
 //prepare port and run server
-const port = 3333
+const port = 3000
 app.listen(port,console.log(`Server is listening port ${port}...`))
 
 
