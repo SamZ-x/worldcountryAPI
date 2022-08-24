aaca58f4bea3171d97cf3e3ee64a6df8e2bf8770
Xiaobin Zhu <sam.zhu992@gmail.com>
2022-08-24 11:28:17 -0600

    -add security middlewares

--- app.js
+++ app.js
@@ -3,6 +3,9 @@
 //import dependencies
 require('dotenv').config()
 require('express-async-errors')
+const helmet = require('helmet')
+const xss = require('xss-clean')                     //sanitize user input coming from POST body, GET queries, and url params. 
+const ratelimiter = require('express-rate-limit')    //limite access 
 
 //import express modules and create server
 const express = require('express')
@@ -21,12 +24,18 @@ const corsSet = require('./middlewares/cors')
 
 
 //************  Middlewares USE ************//
+app.use(
+    ratelimiter({
+    windowMs: 15 * 60 * 1000,  //unit:ms   set to 15mins
+    max:100,                  //limit each IP to 100 request per windowMs 
+  }))
 app.use(express.static('./public'))
 app.use(express.json())
-
+app.use(helmet())
+app.use(xss())
 
 //************  MODULES USE ************//
-app.use(corsSet)
+app.use(corsSet)    //($ npm install cors instead)
 
 app.use('/api/v1/worldcountry', countryRoutes)
 app.use(notfound)
